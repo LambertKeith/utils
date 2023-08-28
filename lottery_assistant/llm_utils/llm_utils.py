f51fc73219e52b700c99357c963ec60db98d754b
lambert keith <2698730244@qq.com>
2023-08-28 23:39:55 +0800

    1

--- lottery_assistant/llm_utils/llm_utils.py
+++ lottery_assistant/llm_utils/llm_utils.py
@@ -51,8 +51,5 @@ def base_chat(message,model="gpt-3.5-turbo-0301"):
       ]
     )
 
-    #print(completion)    
-    
-    #print("completion.usage", completion.usage) 
     print(completion.choices[0].message['content'])  
     return completion.choices[0].message['content']
